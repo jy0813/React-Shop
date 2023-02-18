bcb2825959b2199907e72eb2a6f3698dc1e63e5d
jy0813 <busbug3@gmail.com>
2023-02-18 18:13:00 +0900

    feat: 로그인 로그아웃 구현 및 어드민 사용자 경로보호

---
+++ src/api/firebase.js
@@ -0,0 +1,47 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged } from "firebase/auth";
+import { getDatabase, ref, get } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATA_BASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const provider = new GoogleAuthProvider();
+const database = getDatabase(app);
+
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export function login() {
+  signInWithPopup(auth, provider).catch(error => console.error(error));
+}
+
+export function logout() {
+  signOut(auth).catch(error => console.error(error));
+}
+
+export function onUserStateChange(callback) {
+  onAuthStateChanged(auth, async (user) => {
+    const updatedUser = user ? await adminUser(user) : null;
+    callback(updatedUser)
+  });
+}
+
+async function adminUser(user) {
+
+  return get(ref(database, 'admins'))
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          const admins = snapshot.val();
+          const isAdmin = admins.includes(user.uid);
+          return {...user, isAdmin}
+        }
+        return user;
+  });
+}
--- src/index.js
+++ src/index.js
@@ -3,11 +3,33 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import NotFound from "./pages/NotFound";
+import Home from "./pages/Home";
+import AllProducts from "./pages/AllProducts";
+import NewProduct from "./pages/NewProduct";
+import ProductDetail from "./pages/ProductDetail";
+import MyCart from "./pages/MyCart";
+import ProtectedRoute from "./pages/ProtectedRoute";
 
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<App/>,
+    errorElement:<NotFound/>,
+    children:[
+      {index:true,path:'/', element:<Home/>},
+      {path:'/prodcuts', element:<AllProducts/>},
+      {path:'/prodcuts/new', element:<ProtectedRoute requireAdmin><NewProduct/></ProtectedRoute>},
+      {path:'/prodcuts/:id', element:<ProductDetail/>},
+      {path:'/carts', element:<ProtectedRoute><MyCart/></ProtectedRoute>},
+    ]
+  }
+])
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
